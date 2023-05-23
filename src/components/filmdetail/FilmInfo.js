09c95b04935f37ab2e0938eea722ef593aef034e
vokhanhvy <vonhukhanhvy95@gmail.com>
2023-05-23 23:18:11 +0700

    Fix FilmInfo component

--- src/components/filmdetail/FilmInfo.js
+++ src/components/filmdetail/FilmInfo.js
@@ -28,38 +28,35 @@ dayjs.extend(customParseFormat);
 const { Paragraph, Text } = Typography;
 
 const FilmInfo = () => {
-  
   const location = useLocation();
   const [open, setOpen] = useState(false);
   const [ellipsis, setEllipsis] = useState(true);
 
   const [displayTime, setDisplayTime] = useState(false);
 
-  const onChange = () => {
-    setDisplayTime(true);
-  };
-
   const [filmDetailAPI, setFilmDetailAPI] = useState([]);
   const data = [];
   const [isBusy, setBusy] = useState(true);
 
+  // FOR USING ANTD DATEPICKER
   function addDate(date) {
     const d = new Date(date);
     d.setDate(d.getDate() + 1);
     return d.toLocaleDateString("en-US");
   }
 
-
   const loadFilmDetail = () => {
     setBusy(true);
 
     async function fetchData() {
       setBusy(true);
-      axios.get(`https://localhost:7113/api/Films/${location.state.id}`).then((response) => {
-        setBusy(false);
+      axios
+        .get(`https://localhost:7113/api/Films/${location.state.id}`)
+        .then((response) => {
+          setBusy(false);
 
-        setFilmDetailAPI(response.data);
-      });
+          setFilmDetailAPI(response.data);
+        });
     }
     fetchData();
   };
@@ -89,24 +86,31 @@ const FilmInfo = () => {
     if (data[0].schedules.length > 0) {
       const listSchedule = data[0].schedules;
       const index = listSchedule.length - 1;
-     
-      const lastStartDate = addDate(
-        listSchedule[index].startTime.slice(0, 10)
-      );
-      console.log(lastStartDate);
+
+      const lastStartDate = addDate(listSchedule[index].startTime.slice(0, 10));
 
       disabledDate = (current) => {
         // Can not select days before today and date after last start date
         return (
           current &&
-          (current <= dayjs().startOf("day") ||
-            current >= dayjs(lastStartDate))
+          (current <= dayjs().startOf("day") || current >= dayjs(lastStartDate))
         );
       };
       dateStartShowing = listSchedule[0].startTime.slice(0, 10);
       dateEndShowing = listSchedule[index].startTime.slice(0, 10);
     }
   }
+
+  // DISPLAY TIME WHEN SELECT A DATE
+  const [listTime, setListTime] = useState([]);
+  const onSelectDate = (date, dateString) => {
+    const listSchedule = data[0].schedules;
+    const selectListDatetime = listSchedule.filter(
+      (item) => item.startTime.slice(0, 10) == dateString
+    );
+    setListTime(selectListDatetime);
+    setDisplayTime(true);
+  };
 
   return (
     <>
@@ -127,7 +131,7 @@ const FilmInfo = () => {
               ]}
             />
             <Row>
-              <Col lg={18} xs={24} >
+              <Col lg={18} xs={24}>
                 <Row style={{ margin: "1.5em" }}>
                   <Col lg={8} xs={24}>
                     <div>
@@ -200,7 +204,10 @@ const FilmInfo = () => {
                     <div>
                       <Divider orientation="left">
                         <h2>SCHEDULE</h2>{" "}
-                        <DatePicker disabledDate={disabledDate} onChange={() => onChange()} />
+                        <DatePicker
+                          disabledDate={disabledDate}
+                          onChange={onSelectDate}
+                        />
                       </Divider>
                     </div>
                   </Col>
@@ -210,8 +217,11 @@ const FilmInfo = () => {
                     <Col span={24} type="flex" align="left">
                       <div>
                         <Card title="TIME">
-                          <Tag>09:30</Tag>
-                          <Tag>10:30</Tag>
+                          {listTime.map((item) => (
+                            <Tag color="orange">
+                              {item.startTime.slice(11, 16)}
+                            </Tag>
+                          ))}
                         </Card>
                       </div>
                     </Col>
