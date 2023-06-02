fd17bea3597777d06d99928d5a149bc152015110
Duc Hung <hungklyhongkl@gmail.com>
2023-06-02 16:49:44 +0700

    Fix bug

--- src/component/user_details/Transactions.jsx
+++ src/component/user_details/Transactions.jsx
@@ -2,26 +2,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { TbAlertCircle } from "react-icons/tb";
-import { EyeOutlined } from "@ant-design/icons";
+import { BankOutlined, DollarOutlined, EyeOutlined } from "@ant-design/icons";
 import { Button, Modal, Table } from "antd";
 import Barcode from "react-barcode";
+import { useNavigate } from "react-router-dom";
 
 //Show history transactions of user HungTD34
 function Transactions() {
     const [bookings, setBooking] = useState()
     const [code, setCode] = useState()
     const [data, setData] = useState([])
+    const navigate = useNavigate()
+    const [page, setPage] = useState(1)
+    const [total, setTotal] = useState(0)
     useEffect(() => {
         fecthData()
-    }, [])
+    }, [page])
     const fecthData = async () => {
-        var res = await axios.get("https://localhost:7113/api/Bookings/GetTransactionHistory?page=1&pageSize=10",
+        var res = await axios.get("https://localhost:7113/api/Bookings/GetTransactionHistory?page=" + page + "&pageSize=10",
             {
                 headers: { "Authorization": `Bearer ${sessionStorage.getItem('token')}` }
             })
-
-        console.log(res?.data?.data?.listItem)
-        // setBooking(res?.data?.data?.listItem)
+        setTotal(res?.data?.data?.TotalCount)
+        // console.log(res?.data?.data?.TotalCount)
+        console.log(res?.data?.data?.totalCount)
 
         const list = []
         res?.data?.data?.listItem?.map((booking, index) => (
@@ -32,7 +36,8 @@ function Transactions() {
                 totalPrice: booking?.totalPrice,
                 status: booking?.status == "Success" ? <Button style={{ width: "150px" }} type="primary">Thành công</Button> :
                     booking?.status == "Pending" ? <Button style={{ width: "150px" }}>Chưa thanh toán</Button> : <Button type="primary" danger style={{ width: "150px" }}>Thất bại</Button>,
-                function: booking?.status == "Success" && <EyeOutlined onClick={() => showBarcode(booking)} id={1} />
+                function: booking?.status == "Success" ? <Button type="primary" onClick={() => showBarcode(booking)} id={1}>Lấy vé</Button> :
+                    booking?.status == "Pending" && <Button type="primary" onClick={() => continuePayment(booking?.urlPayment)}>Thanh toán ngay</Button>
                 // barcode : <div className = "barcode"><Barcode height={50} width={1} displayValue = {false} value={booking} /></div>
             })
         ))
@@ -43,6 +48,10 @@ function Transactions() {
     const showBarcode = (booking) => {
         setCode("Booking id : " + booking?.id)
         showModal()
+    }
+
+    const continuePayment = (url) => {
+        window.location = url
     }
 
     const columns = [
@@ -87,7 +96,20 @@ function Transactions() {
     return (
         <>
             <div className="table" style={{ minHeight: "45vh" }}>
-                <Table columns={columns} dataSource={data} size="middle" />
+                <Table
+                    columns={columns}
+                    dataSource={data}
+                    size="middle"
+                    pagination={{
+                        position: ["bottomCenter"],
+                        current: page,
+                        total: total,
+                        // showSizeChanger: true,
+                        onChange: (page) => {
+                            setPage(page)
+                        }
+                    }}
+                />
             </div>
 
             <Modal title="Mã booking" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
